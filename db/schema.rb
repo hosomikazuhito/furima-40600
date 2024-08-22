f2b790db0bb1a286b2e07fd606d5bad779c2abf0
hosomikazuhito <hosomi95@gmail.com>
2024-08-22 16:20:28 +0900

    ユーザー管理機能修正依頼2

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_08_17_045306) do
+ActiveRecord::Schema[7.0].define(version: 2024_08_14_065639) do
   create_table "users", charset: "utf8mb3", force: :cascade do |t|
     t.string "nickname", null: false
     t.string "email", default: "", null: false
@@ -25,7 +25,6 @@ ActiveRecord::Schema[7.0].define(version: 2024_08_17_045306) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -15,5 +15,35 @@ RSpec.describe User, type: :model do
       @user.valid?
       expect(@user.errors.full_messages).to include "Email can't be blank"
     end
+    it 'passwordが空では登録できない' do
+      @user.password = ''
+      @user.valid?
+      expect(@user.errors.full_messages).to include "Password can't be blank"
+    end
+    it 'last_nameが空では登録できない' do
+      @user.last_name = ''
+      @user.valid?
+      expect(@user.errors.full_messages).to include "Last name can't be blank"
+    end
+    it 'first_nameが空では登録できない' do
+      @user.first_name = ''
+      @user.valid?
+      expect(@user.errors.full_messages).to include "First name can't be blank"
+    end
+    it 'kana_last_nameが空では登録できない' do
+      @user.kana_last_name = ''
+      @user.valid?
+      expect(@user.errors.full_messages).to include "Kana last name can't be blank"
+    end
+    it 'kana_first_nameが空では登録できない' do
+      @user.kana_first_name = ''
+      @user.valid?
+      expect(@user.errors.full_messages).to include "Kana first name can't be blank"
+    end
+    it 'birthが空では登録できない' do
+      @user.birth = ''
+      @user.valid?
+      expect(@user.errors.full_messages).to include "Birth can't be blank"
+    end
   end
 end
