ce5378f9680d907c68f9bdb27750b2cc71f10990
hosomikazuhito <hosomi95@gmail.com>
2024-09-06 21:39:36 +0900

    商品情報編集機能完成

--- app/controllers/goods_controller.rb
+++ app/controllers/goods_controller.rb
@@ -1,8 +1,10 @@
 class GoodsController < ApplicationController
-
+  before_action :authenticate_user!, except: [:index, :show]
+  before_action :set_good, only: [:edit, :update, :destroy]
+  before_action :check_owner, only: [:edit, :update, :destroy]
 
   #before_action :set_good, except: [:index, :new, :create]
-  before_action :authenticate_user!, except: [:index, :show]
+  
   #before_action :contributor_confirmation, only: [:edit, :update, :destroy]
   
   
@@ -15,6 +17,11 @@ class GoodsController < ApplicationController
   def index
   @goods = Good.order(created_at: :desc)
   @goods_exist = @goods.any?
+  end
+
+  def edit
+    @good = Good.find(params[:id])
+    
   end
 
   def show
@@ -35,13 +42,17 @@ class GoodsController < ApplicationController
   end
   
   
-  #def update
-  #if @good.update(good_params)
-  #redirect_to @good, notice: 'Good was successfully updated.'
-  #else
-  #render :edit, status: :unprocessable_entity
-  #end
-  #end
+  
+  def update
+    @good = Good.find(params[:id])
+    if @good.update(good_params)
+      redirect_to @good, notice: '商品が更新されました。'
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+  
+  
   
   
   #def set_good
@@ -53,14 +64,25 @@ class GoodsController < ApplicationController
   
   
   def good_params
-    params.require(:good).permit(:name, :name_explanation, :category_id, :condition_id, :delivery_id, :aria_id, :shipment_id, :price, :image)
+    params.require(:good).permit(:name, :name_explanation, :category_id, :condition_id, :delivery_id, :aria_id, :shipment_id, :price)
   end
   
-  
+  def set_good
+    @good = Good.find(params[:id])
+  end
+
+  def check_owner
+    if @good.user != current_user
+      redirect_to root_path, alert: "あなたはこの商品の編集権限を持っていません。"
+    end
+  end
+end
+
+
   
   #def configure_permitted_parameters
   #devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
   #end
   
   
-  end
+ 
