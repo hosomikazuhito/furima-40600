a0a18c9de578c46f9895909b9b429fcf51b99118
hosomikazuhito <hosomi95@gmail.com>
2024-09-06 16:25:16 +0900

    商品詳細表示機能完成

--- app/models/delivery.rb
+++ app/models/delivery.rb
@@ -1,8 +1,8 @@
 class Delivery < ActiveHash::Base
   self.data = [
-      { id: 1, delivery: '---' },
-      { id: 2, delivery: '着払い(購入者負担)' },
-      { id: 3, delivery: '送料込み(出品者負担)' },
+      { id: 1, name: '---' },
+      { id: 2, name: '着払い(購入者負担)' },
+      { id: 3, name: '送料込み(出品者負担)' },
       
     ]
   
--- app/models/good.rb
+++ app/models/good.rb
@@ -1,7 +1,16 @@
 class Good < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :user
   #has_one :buy
   has_one_attached :image
+  
+  
+  belongs_to :category
+  belongs_to :condition
+  belongs_to :delivery   
+  belongs_to :aria   
+  belongs_to :shipment
+  
   
   def delivery_method
     case delivery_id
