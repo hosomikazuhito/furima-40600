887e2768b05c6e6b9106581db97956eb90c176f1
hosomikazuhito <hosomi95@gmail.com>
2024-09-07 17:30:42 +0900

    商品削除機能修正

--- app/controllers/goods_controller.rb
+++ app/controllers/goods_controller.rb
@@ -1,7 +1,7 @@
 class GoodsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
   before_action :set_good, only: [:edit, :update, :show, :destroy]
-  before_action :check_owner, only: [:edit, :update]
+  before_action :check_owner, only: [:edit, :update, :destroy]
 
   #before_action :set_good, except: [:index, :new, :create]
   
